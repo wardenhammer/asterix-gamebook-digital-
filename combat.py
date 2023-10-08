a048a09fb197a35bff6b991c13b5f21a41f2a6bc
Kevin Malley <kevin.malley@outlook.com>
2023-10-08 16:18:04 +0100

    Added loop for comabt

--- combat.py
+++ combat.py
@@ -15,8 +15,16 @@ asterix = Hero(15,15,15)
 legionaire = Enemy(12)
 
 # Start combat
-diceRoll = random.randint(1, 6)
-print(f"dice={diceRoll}")
-legionaire.fightingfitness = legionaire.fightingfitness - diceRoll
-print(vars(asterix))
-print(vars(legionaire))
+while(asterix.fightingfitness > 0 and legionaire.fightingfitness > 0):
+    print("Asterix hits the Legionire...")
+    diceRoll = random.randint(1, 6)
+    print(f"dice={diceRoll}")
+    legionaire.fightingfitness = legionaire.fightingfitness - diceRoll
+    print(vars(asterix))
+    print(vars(legionaire))
+    print("Legionire hits Asterix")
+    diceRoll = random.randint(1, 6)
+    print(f"dice={diceRoll}")
+    asterix.fightingfitness = asterix.fightingfitness - diceRoll
+    print(vars(asterix))
+    print(vars(legionaire))
