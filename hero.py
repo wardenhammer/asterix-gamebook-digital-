4e04b99d2be3de9e867e31421192252be072a1f9
Kevin Malley <kevin.malley@outlook.com>
2024-03-17 16:41:44 +0000

    Added setters for all the properties

--- hero.py
+++ hero.py
@@ -1,20 +1,31 @@
 class Hero:
     def __init__(self, fighting_fitness, charm, skill):
-        self.fighting_fitness = fighting_fitness
-        self.charm = charm
-        self.skill = skill
-        self.sestertii
+        self._fighting_fitness = fighting_fitness
+        self._charm = charm
+        self._skill = skill
+        self._sestertii = 0
 
     @property
     def fighting_fitness(self):
         return self.fighting_fitness
+    @fighting_fitness.setter
+    def fighting_fitness(self, value):
+        self._fighting_fitness = value
     @property
     def charm(self):
         return self.charm
+    @charm.setter
+    def charm(self, value):
+        self._charm = value
     @property
     def skill(self):
         return self.skill
-    
+    @skill.setter
+    def skill(self, value):
+        self._skill = value
     @property
     def sestertii(self):
         return self.sestertii
+    @sestertii.setter
+    def sestertii(self, value):
+        self._sestertii = value
