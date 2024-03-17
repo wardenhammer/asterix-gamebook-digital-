52d7cc7b364d1bdb4e227eff12bf154b174dfd85
Kian Malley <kian.malley@outlook.com>
2024-03-17 16:49:24 +0000

    renamed fighting fitness and combat

--- combat.py
+++ combat.py
@@ -7,16 +7,16 @@ asterix = Hero(15,15,15)
 legionaire = Enemy(12)
 
 # Start combat
-while(asterix.fightingfitness > 0 and legionaire.fightingfitness > 0):
+while(asterix.fighting_fitness > 0 and legionaire.fighting_fitness > 0):
     print("Asterix hits the Legionire...")
     diceRoll = random.randint(1, 6)
     print(f"dice={diceRoll}")
-    legionaire.fightingfitness = legionaire.fightingfitness - diceRoll
+    legionaire.fighting_fitness = legionaire.fighting_fitness - diceRoll
     print(vars(asterix))
     print(vars(legionaire))
     print("Legionire hits Asterix")
     diceRoll = random.randint(1, 6)
     print(f"dice={diceRoll}")
-    asterix.fightingfitness = asterix.fightingfitness - diceRoll
+    asterix.fighting_fitness = asterix.fighting_Sfitness - diceRoll
     print(vars(asterix))
     print(vars(legionaire))
